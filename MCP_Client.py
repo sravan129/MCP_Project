b64bdf812c7c6f6804e1ffd57da69f4c68c12bb3
sravan129 <151582227+sravan129@users.noreply.github.com>
2025-11-10 15:11:48 +0530

    Add files via upload

--- MCP_Client.py
+++ MCP_Client.py
@@ -1,8 +1,16 @@
 import asyncio
 import streamlit as st
-from langchain_openai import ChatOpenAI
+from langchain_classic import ChatOpenAI
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from langchain.agents import create_agent
+
+with st.sidebar:
+    st.title("Provide API")
+    OPENAI_API_KEY=st.text_input("OpenAI API key",type="password")
+
+if not OPENAI_API_KEY:
+    st.text("Enter API")
+    st.stop()
 
 client=MultiServerMCPClient({
  "tools":{
@@ -14,7 +22,7 @@ client=MultiServerMCPClient({
 
 tools = asyncio.run(client.get_tools())
 
-llm = ChatOpenAI(model="gpt-4o")
+llm = ChatOpenAI(model="gpt-4o",api_key="OPENAI_API_KEY")
 
 agent = create_agent(llm, tools)
 
